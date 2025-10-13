fd759d456efacec74fccabee8bbb87f4b125ab70
truggiero-rucavi <tomas@rucavi.com>
2025-10-13 16:47:37 -0300

    Push base y visualizador/colecciones.html

---
+++ ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/DdsiGrupo33ServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddsigrupo33serverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdsiGrupo33ServerSideApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DdsiGrupo33ServerSideApplication.class, args);
+  }
+
+}
---
+++ ddsi-grupo-33-server-side/src/test/java/com/example/ddsigrupo33serverside/DdsiGrupo33ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddsigrupo33serverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdsiGrupo33ServerSideApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
