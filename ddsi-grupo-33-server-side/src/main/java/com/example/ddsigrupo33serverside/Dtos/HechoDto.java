7fcc63b438f3f738c9426ed8b891b1ce54cfd41b
Juan Ignacio Molina <juanmolin@frba.utn.edu.ar>
2025-10-15 15:45:36 -0300

    E5 visualizador (#1)
    * Subo plantillas dinamicas de ver hechos de una coleccion, y ver un hecho
    
    * Fix para que se puedan ver los hechos individuales correctamente
    
    * Refactor para que los hechos tomen longitud y latitud de HechoDto para generar el mapa dinamicamente
    
    * Subo fix para que se vea el modal de solicitud de eliminacion
    
    * Subo fix para que se vea el componente de filtros de hechos en una coleccion
    
    * Agrego componente visual para seleccionar la forma de navegacion irrestricta o curada, falta dinamizarlo

--- ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Dtos/HechoDto.java
+++ ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Dtos/HechoDto.java
@@ -2,9 +2,18 @@ package com.example.ddsigrupo33serverside.Dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import java.util.Set;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 public class HechoDto {
+  public UUID id;
   public String titulo;
+  private String fecha_acontecimiento;
+  private String ubicacion;
+  private String fuente;
+  private Set<String> categorias;
+  private String longitud;
+  private String latitud;
 }
