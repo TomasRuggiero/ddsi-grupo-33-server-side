85c4d25aa78f4f1d8420b39f47f4657d44eada01
TomasRuggiero <97413594+TomasRuggiero@users.noreply.github.com>
2025-12-03 18:18:04 -0300

    Fix subir hecho sin multimedia (#6)
    Co-authored-by: truggiero-rucavi <tomas@rucavi.com>

--- ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Dtos/HechoDto.java
+++ ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Dtos/HechoDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
@@ -16,6 +18,7 @@ public class HechoDto {
   private String titulo;
   private String descripcion;
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+  @DateTimeFormat(pattern = "yyyy-MM-dd") // Esto soluciona el binding del formulario web  private Date fecha;
   private Date fecha;
   private String fuente;
   private Set<String> categorias;
