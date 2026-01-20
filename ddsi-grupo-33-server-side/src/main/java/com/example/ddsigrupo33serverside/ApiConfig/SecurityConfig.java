d6f3337f6888ca6342a26e895ee0a8a5882982dd
truggiero-rucavi <tomas@rucavi.com>
2026-01-20 19:16:06 -0300

    Disable csrf

--- ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/ApiConfig/SecurityConfig.java
+++ ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/ApiConfig/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableMethodSecurity(prePostEnabled = true)
@@ -24,6 +25,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
             // Recursos estáticos y login público
             .requestMatchers("/**").permitAll()
