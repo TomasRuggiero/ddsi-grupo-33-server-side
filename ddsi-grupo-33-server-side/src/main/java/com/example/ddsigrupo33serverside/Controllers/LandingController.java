03f3f8a27d86b7c0521f871d925d3ab6faa0e42e
Juan Ignacio Molina <juanmolin@frba.utn.edu.ar>
2026-01-27 21:23:14 -0300

    Agrego logica de colecciones del home para que ya no estén harcodeadas

--- ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Controllers/LandingController.java
+++ ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Controllers/LandingController.java
@@ -1,13 +1,31 @@
 package com.example.ddsigrupo33serverside.Controllers;
 
+import com.example.ddsigrupo33serverside.Dtos.ColeccionDto;
+import com.example.ddsigrupo33serverside.Services.ColeccionApiClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class LandingController {
 
+  private final ColeccionApiClient coleccionApiClient;
+
   @GetMapping("/")
-  public String landing(){
-    return "/index";
+  public String landing(Model model) {
+
+    List<ColeccionDto> colecciones = coleccionApiClient.getTodasLasColecciones();
+
+    List<ColeccionDto> coleccionesHome = colecciones.stream()
+        .limit(4)
+        .toList();
+
+    model.addAttribute("colecciones", coleccionesHome);
+
+    return "index";
   }
 }
+
