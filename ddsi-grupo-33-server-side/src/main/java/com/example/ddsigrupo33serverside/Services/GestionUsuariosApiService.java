56bdb9e1edc6a10aab70b18e66ae9e327f078948
truggiero-rucavi <tomas@rucavi.com>
2026-02-09 20:23:47 -0300

    Correccion vars de entorno

--- ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Services/GestionUsuariosApiService.java
+++ ddsi-grupo-33-server-side/src/main/java/com/example/ddsigrupo33serverside/Services/GestionUsuariosApiService.java
@@ -23,7 +23,7 @@ public class GestionUsuariosApiService {
   @Autowired
   public GestionUsuariosApiService(
       WebApiCallerService webApiCallerService,
-      @Value("${auth.service.url}") String authServiceUrl){
+      @Value("${BACKEND_URL:http://localhost:8080}") String authServiceUrl){
     this.webClient = WebClient.builder().
     baseUrl(authServiceUrl)
   .build();
